57fe49b105fb891f46fdcfdcd83778bbada7033d
CyrilArtFX <ccrozat@artfx.fr>
2022-11-04 10:35:17 +0100

    [added] boss model and animations

---
+++ FightingGame/Assets/Scripts/EmployeEventRelay.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EmployeEventRelay : MonoBehaviour
+{
+    [SerializeField]
+    Employe employe;
+
+
+    public void Punch()
+    {
+        employe.Punch();
+    }
+
+    public void Stomp()
+    {
+        employe.Stomp();
+    }
+
+    public void ResetAttacks()
+    {
+        employe.ResetAttacks();
+    }
+
+    public void ActivateUlti()
+    {
+        employe.ActivateUlti();
+    }
+
+    public void Ulti()
+    {
+        employe.Ulti();
+    }
+}
